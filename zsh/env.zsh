360507dc224bddd63e361beb6089e26e2678ab4e
motivic <johnson.jia@live.com>
2016-08-27 06:48:22 -0400

    added support for pyenv

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,5 +1,10 @@
 # https://github.com/motivic/dotfiles
 
-PATH="$(ruby -e 'print Gem.user_dir')/bin:$PATH"
+export PATH="$(ruby -e 'print Gem.user_dir')/bin:$PATH"
 export XDG_CONFIG_HOME="$HOME/.config"
 # export TERM="xterm-256color"
+
+# pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+
