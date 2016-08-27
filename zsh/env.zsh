07e4ad515a0a7af9ddd8e89b27ac306e3b8466d5
motivic <johnson.jia@live.com>
2016-08-27 07:00:55 -0400

    removed env for pyenv

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -3,8 +3,3 @@
 export PATH="$(ruby -e 'print Gem.user_dir')/bin:$PATH"
 export XDG_CONFIG_HOME="$HOME/.config"
 # export TERM="xterm-256color"
-
-# pyenv
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-
