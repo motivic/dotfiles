adf1696033bbcfd87a351e4369ec091df5723bbb
motivic <johnson.jia@live.com>
2016-10-26 23:55:51 -0400

    setup pyenv path and initialize virtualenv, added cuDNN path

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -5,7 +5,15 @@ export XDG_CONFIG_HOME="$HOME/.config"
 # export TERM="xterm-256color"
 
 # pyenv
+# pyenv path
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
 # pyenv enable shims and autocompletion
 eval "$(pyenv init -)"
 # pyenv build mirrors
 export PYTHON_BUILD_MIRROR_URL="http://yyuu.github.io/pythons"
+# pyenv auto-activate pyenv virtualenv
+eval "$(pyenv virtualenv-init -)"
+
+# cuDDN path
+export LD_LIBRARY_PATH="/opt/cuda/lib64:$LD_LIBRARY_PATH"
