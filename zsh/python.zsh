3f5533f198c9bc4a48899be3ca82292726932fdf
Johnson <johnson.jia@capitalone.com>
2016-11-23 15:02:06 -0500

    updated env.zsh and python.zsh

--- zsh/python.zsh
+++ zsh/python.zsh
@@ -1,4 +1,15 @@
 # https://github.com/motivic/dotfiles
 
+# pyenv
+# pyenv path
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+# pyenv enable shims and autocompletion
+eval "$(pyenv init -)"
+# pyenv build mirrors
+export PYTHON_BUILD_MIRROR_URL="http://yyuu.github.io/pythons"
+# pyenv auto-activate pyenv virtualenv
+eval "$(pyenv virtualenv-init -)"
+
 # virtualenvwrapper
 source $PYENV/shims/virtualenvwrapper.sh
