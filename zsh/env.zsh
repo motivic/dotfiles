9c441ae5b9435fba8ba7e44dd40618a90a242f98
motivic <johnson.jia@live.com>
2017-12-20 12:12:08 -0500

    update git config and zsh env

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -24,9 +24,6 @@ export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}
 # cuDNN path
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
-# libsvm path
-export LIBSVM=$HOME/code/miscellaneous/libsvm
-
 # IntelliJ Ultimate path
 export PATH=$HOME/.local/share/umake/ide/idea-ultimate/bin:$PATH
 
