9f41e55b8405f49bb62d110a3fcc1594f960bb23
Johnson <jia.johnson@jd.com>
2018-09-13 11:31:08 -0700

    added aliases and environment variables for CLI tools mentioned in https://remysharp.com/2018/08/23/cli-improved

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,4 @@
-# https://github.com/motivic/dotfiles
+#https://github.com/motivic/dotfiles
 
 # global aliases
 alias -g ..='cd ..'
@@ -16,3 +16,16 @@ alias vim=nvim
 
 # uncompress git blob
 alias git-deflate="perl -MCompress::Zlib -e 'undef $/; print uncompress(<>)'"
+
+## CLI tools as recommended by https://remysharp.com/2018/08/23/cli-improved
+# [bat](https://github.com/sharkdp/bat)
+alias cat='bat'
+
+# [prettyping](https://github.com/denilsonsa/prettyping)
+alias ping='/Users/johnsonjia/code/misc/prettyping'
+
+# preview via [fzf](https://github.com/junegunn/fzf)
+alias preview="fzf --preview 'bat --color \"always\" {}'"
+
+# [tldr](https://tldr.sh/)
+alias help='tldr'
--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,3 +1,14 @@
 # https://github.com/motivic/dotfiles
 
 # PATH="$(ruby -e 'print Gem.user_dir')/bin:$PATH"
+
+# Overwrite XCode git
+export PATH=/usr/local/opt/git/bin:$PATH
+
+
+## CLI tools as recommended by https://remysharp.com/2018/08/23/cli-improved
+# [bat](https://github.com/sharkdp/bat) style
+export BAT_THEME="GitHub"
+
+# add support for ctrl+o to open selected file in VS Code
+export FZF_DEFAULT_OPTS="--bind='ctrl-o:execute(nvim {})+abort'"
